c017db893412b7383e9584a3680648ddb557be3d
markhdwight <markhdwight@gmail.com>
2017-09-01 18:16:51 -0500

    completed assignment

--- Employee.js
+++ Employee.js
@@ -3,14 +3,25 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name,title,salary) {
+    this.name = name
+    this.title = title
+    this.salary = salary
   }
 
-  // TODO ???
-}
+  promote(newTitle,newSalary){
+    this.title = newTitle
+    this.salary = newSalary
+  }
 
-// TODO ???
+  static parseFromFilePath(filePath){
+    let file = fs.readFileSync(filePath)
+    let empData = JSON.parse(file)
+    let employee = new Employee(empData.name,empData.title,empData.salary)
+
+    return employee
+  }
+}
 
 module.exports = {
   Employee
